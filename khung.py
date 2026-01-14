c8509b537d069f35863e2ca3b9eb346e98341115
haocole <tbysnanyoel@gmail.com>
2026-01-14 21:34:24 +0700

    Cập nhật khung sườn mới

---
+++ khung.py
@@ -0,0 +1,19 @@
+# Vẽ phần tiêu đề trên cùng
+print("**************************************************")
+print("* Chuong Trinh Hoc Thong Minh             *")
+print("**************************************************")
+
+# Vẽ thanh Menu
+print("====================== MENU ======================")
+
+# Vẽ khung chứa các lựa chọn
+print("__________________________________________________")
+print("| Xin vui long chon :                            |")
+print("| 1. Xem lich                                    |")
+print("| 2. Tinh luong                                  |")
+print("| 3. Xem luong                                   |")
+print("| 4. Xem thong tin nhan vien                     |")
+print("| 5. Tinh diem cua hoc sinh                      |")
+print("| 6. Thoat chuong trinh                          |")
+print("|                                                |")
+print("|________________________________________________|")
