a96c388da908401b6ff88babd638902bb8a0a837
haocole <tbysnanyoel@gmail.com>
2026-01-14 21:21:28 +0700

    Them file tinh luong

---
+++ tinh_luong.py
@@ -0,0 +1,7 @@
+hour = float(input("Nhập số giờ làm: "))
+rate = float(input("Nhập số lương giờ: "))
+if hour > 40:
+    total_salary = 40 * rate + (hour - 40) * rate * 1.5
+else:
+    total_salary = hour * rate
+print(f'Tống lưởng là: {total_salary}')
